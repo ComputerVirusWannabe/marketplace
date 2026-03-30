0d926e8e7e7228e538b851f382e5abdc4b4b340c
Vien Pham <bfb9jf@virginia.edu>
2026-03-29 21:28:03 -0400

    for render #4

--- project_a03/settings.py
+++ project_a03/settings.py
@@ -34,7 +34,7 @@ DEBUG = False
 # DEBUG = os.getenv("DEBUG") == "True"
 
 # ADD HEROKU APP URL HERE
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'cs-3240-a03-04435f33f31b.herokuapp.com', 'https://marketplace-5ask.onrender.com/']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', 'cs-3240-a03-04435f33f31b.herokuapp.com', 'marketplace-5ask.onrender.com/']
 
 CSRF_TRUSTED_ORIGINS = [
     "https://cs-3240-a03-04435f33f31b.herokuapp.com","https://marketplace-5ask.onrender.com"
