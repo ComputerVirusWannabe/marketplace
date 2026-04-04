2bec82a89300cc505984b812a41c247db19e3d71
Vien Pham <bfb9jf@virginia.edu>
2026-04-03 23:37:07 -0400

    update for render #6

--- project_a03/settings.py
+++ project_a03/settings.py
@@ -99,14 +99,23 @@ WSGI_APPLICATION = "project_a03.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=not DEBUG,
-    )
-}
+if "DATABASE_URL" in os.environ:
+    # Production (Render use Postgres)
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        )
+    }
+else:
+    # Local development (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
